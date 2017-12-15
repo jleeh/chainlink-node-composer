2361a97e83c1d9276f6ab0fb98fc7141c2bcc742
Jonny Huxtable <jonathan.huxtable@skybettingandgaming.com>
2017-12-15 11:16:05 +0000

    Added initialisation check to node bootup

--- chainlink/entrypoint.sh
+++ chainlink/entrypoint.sh
@@ -1,11 +1,17 @@
-# Confirm to create coordinator keys
-bundle exec
-echo "Y" | rake oracle:initialize > "/initialise.out"
-cat /initialise.out
+#!/bin/bash
 
-# Output the key-pairs to a file also on the host
-grep 'SECRET:' "/initialise.out" > "/host/keypair"
-grep 'KEY:' "/initialise.out" >> "/host/keypair"
+# Has this node been initialised?
+FILE=/initialise.out
+if [ ! -f $FILE ]; then
+    # Confirm to create coordinator keys
+    bundle exec
+    echo "Y" | rake oracle:initialize > $FILE
+    cat $FILE
+
+    # Output the key-pairs to a file also on the host
+    grep 'SECRET:' $FILE > "/host/keypair"
+    grep 'KEY:' $FILE >> "/host/keypair"
+fi
 
 # Start the node
 foreman start
